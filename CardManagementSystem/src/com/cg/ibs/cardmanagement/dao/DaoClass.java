fca64d45865b78958259af340adeb8643279cd98
pragya633 <56021031+pragya633@users.noreply.github.com>
2019-10-02 15:45:49 +0530

    Update DaoClass.java

--- CardManagementSystem/src/com/cg/ibs/cardmanagement/dao/DaoClass.java
+++ CardManagementSystem/src/com/cg/ibs/cardmanagement/dao/DaoClass.java
@@ -1,5 +1,23 @@
 package com.cg.ibs.cardmanagement.dao;
 
 public class DaoClass {
+public CardManagement(){
+
+    HashMap<String, ArrayList<String>> User_Details = new ArrayList<String, ArrayList<String>>();
+
+	HashMap<String, > Customer_Details = new ArrayList<String, ArrayList<String>>();	
+
+	HashMap<String, > Credit_Card_Details = new ArrayList<String, ArrayList<String>>();
+
+	HashMap<String, > Debit_Card_Details = new ArrayList<String, ArrayList<String>>();
+
+	HashMap<String, > Query_Type = new ArrayList<String, ArrayList<String>>();
+
+	HashMap<String, > Query_Details = new ArrayList<String, ArrayList<String>>();
+
+      CardService =new CardService();
+
+    }
+
 
 }
